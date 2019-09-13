b3d514a3fb88cc6f5f15eea63eba9c61d294107d
Matthew Robinson <matt_robinson1@yahoo.com>
2019-09-13 12:28:16 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -16,10 +16,15 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-     text.match(/^[A-Z].*[$.?!]/)
+     if text.match(/\A[A-Z]/) && text.match(/\p{P}\z/)
+    true
+  else
+    false
+  end
+end
    
 
-end
+
 
 def valid_phone_number?(phone)
 
